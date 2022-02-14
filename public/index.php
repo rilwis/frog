c2f8b060f0104bd4fa22a9f97b9169b9584ddcf1
Anh Tran <anhtnt@elightup.com>
2022-02-14 15:08:31 +0700

    Output both terminal and browser

--- public/index.php
+++ public/index.php
@@ -5,7 +5,7 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Frog</title>
-	<link rel="stylesheet" href="https://unpkg.com/bamboo.css">
+	<link rel="stylesheet" href="https://unpkg.com/bamboo.css/dist/light.min.css">
 </head>
 <body>
 	<div id="data"></div>
--- src/Server.php
+++ src/Server.php
@@ -7,9 +7,11 @@ use SplObjectStorage;
 use Exception;
 
 class Server implements MessageComponentInterface {
-	protected $clients;
+	private $encoder;
+	private $clients;
 
 	public function __construct() {
+		$this->encoder = new PHPEncoder;
 		$this->clients = new SplObjectStorage;
 	}
 
@@ -20,19 +22,21 @@ class Server implements MessageComponentInterface {
 	public function onMessage( ConnectionInterface $from, $message ) {
 		$variables = json_decode( $message, true );
 
-		ob_start();
-		$encoder = new PHPEncoder;
-		foreach ( $variables as $variable ) {
-			echo $encoder->encode( $variable ), "\n";
-		}
-		$message = ob_get_clean();
-
 		// Output to terminal.
-		echo "\n", str_repeat( '-', 40 ), "\n", $message, "\n";
+		$output = '';
+		foreach ( $variables as $variable ) {
+			$output .= $this->encoder->encode( $variable ) . "\n\n";
+		}
+		echo "\n", str_repeat( '-', 40 ), "\n\n", $output, "\n";
 
+		// Output to the browser.
+		$output = '';
+		foreach ( $variables as $variable ) {
+			$output .= $this->highlight( $this->encoder->encode( $variable ) ) . "\n\n";
+		}
 		foreach ( $this->clients as $client ) {
 			if ( $from !== $client ) {
-				$client->send( $message );
+				$client->send( $output );
 			}
 		}
 	}
@@ -43,5 +47,22 @@ class Server implements MessageComponentInterface {
 
 	public function onError( ConnectionInterface $connection, Exception $e ) {
 		$connection->close();
+	}
+
+	/**
+	 * @link https://www.php.net/highlight_string
+	 */
+	private function highlight( $text ) {
+		$text = trim( $text );
+		$text = highlight_string( "<?php " . $text, true );  // highlight_string() requires opening PHP tag or otherwise it will not colorize the text
+		$text = trim( $text );
+		$text = preg_replace( "|^\\<code\\>\\<span style\\=\"color\\: #[a-fA-F0-9]{0,6}\"\\>|", "", $text, 1 );  // remove prefix
+		$text = preg_replace( "|\\</code\\>\$|", "", $text, 1 );  // remove suffix 1
+		$text = trim( $text );  // remove line breaks
+		$text = preg_replace( "|\\</span\\>\$|", "", $text, 1 );  // remove suffix 2
+		$text = trim( $text );  // remove line breaks
+		$text = preg_replace( "|^(\\<span style\\=\"color\\: #[a-fA-F0-9]{0,6}\"\\>)(&lt;\\?php&nbsp;)(.*?)(\\</span\\>)|", "\$1\$3\$4", $text );  // remove custom added "<?php "
+
+		return $text;
 	}
 }
