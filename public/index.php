e2d8c6406b4340e4033b020b634bd612db71abf4
Anh Tran <anhtnt@elightup.com>
2022-02-15 15:57:15 +0700

    Add clear button to the log page

--- public/index.php
+++ public/index.php
@@ -3,21 +3,42 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="viewport" content="width=device-width,initial-scale=1">
 	<title>Frog</title>
 	<link rel="stylesheet" href="https://unpkg.com/bamboo.css/dist/light.min.css">
+	<style>
+		body {
+			margin-top: 2em;
+		}
+		.hidden {
+			display: none;
+		}
+		#clear {
+			position: fixed;
+			top: 1em;
+			right: 1em;
+		}
+	</style>
 </head>
 <body>
-	<div id="data"></div>
+	<p>Listening...</p>
+	<div id="result"></div>
+	<button id="clear" class="hidden">Clear</button>
 
 	<script>
-	const show = data => {
-		const el = document.querySelector( '#data' );
-		el.innerHTML += `<pre>${ data }</pre>`;
-	};
-
+	const clearBtn   = document.querySelector( '#clear' );
+	const result     = document.querySelector( '#result' );
 	const connection = new WebSocket('ws://<?= HOST ?>:<?= PORT ?>');
-	connection.onmessage = e => show( e.data );
+
+	connection.onmessage = e => {
+		result.innerHTML += `<pre>${ e.data }</pre>`;
+		clearBtn.classList.remove( 'hidden' );
+	}
+
+	clearBtn.addEventListener( 'click', () => {
+		result.innerHTML = '';
+		clearBtn.classList.add( 'hidden' );
+	} );
 	</script>
 </body>
 </html>
